833099c603083dcffa988d59894219f0bd791c70
SeolMyeongJae <tjfaudwo0818@gmail.com>
2022-04-06 09:44:26 +0900

    fixed untracked files

--- gbt/src/main/java/ksafinalproject/gbt/HomeController.java
+++ gbt/src/main/java/ksafinalproject/gbt/HomeController.java
@@ -1,12 +1,45 @@
 package ksafinalproject.gbt;
 
+import java.io.File;
+import java.io.InputStream;
+import javax.swing.filechooser.FileSystemView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class HomeController {
-    @GetMapping
-    public String home() {
+    @GetMapping("/admin/home")
+    public String index() {
         return "home";
+    }
+
+    @GetMapping("/admin/challenge")
+    public String test() {return "challenge";}
+
+    @GetMapping("/admin/community")
+    public String community() {return "community";}
+
+    @GetMapping("/admin/challenge/save")
+    public String challengeSave() {
+        return "challenge-save";
+    }
+
+    @PostMapping("/upload")
+    public String upload(String title, String name, String content, @RequestParam("files")MultipartFile file) throws Exception {
+//        String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
+//        String basePath = rootPath + "/" + "single";
+//
+//        String filePath = basePath + "/" + file.getOriginalFilename();
+//
+//        File dest = new File(filePath);
+//        file.transferTo(dest);
+//        ObjectMeta
+//        String fileName = file.getOriginalFilename();
+//        file.getContentType();
+//        InputStream inputStream = file.getInputStream();
+        return "uploaded";
     }
 }
